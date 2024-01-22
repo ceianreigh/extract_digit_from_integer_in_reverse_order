b86f86e457300b09032fc5ce78babe0e1c10af5c
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 20:29:58 +0800

    Write the code that will reverse the number using while loop

--- extract_digit_from_integer_in_reverse_order.py
+++ extract_digit_from_integer_in_reverse_order.py
@@ -7,6 +7,13 @@ number = int(input("Enter a number: "))
 
 # print the entered number
 print(number)
-# reverse the number
 
+# reverse the number
+reversed_number = 0
+while number > 0:
+    remainder = number % 10
+    reversed_number = (reversed_number * 10) + remainder
+    number = number // 10
+
+print(reversed_number)
 # print the reversed number with spaces between each digit
