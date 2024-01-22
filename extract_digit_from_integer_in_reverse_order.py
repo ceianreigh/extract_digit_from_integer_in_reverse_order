ac079e15dd14d9e53582883a6154b767be057659
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 20:21:31 +0800

    Add the instructions and pseudocode

---
+++ extract_digit_from_integer_in_reverse_order.py
@@ -0,0 +1,11 @@
+# Write a Program to extract each digit from an integer in the reverse order.
+
+# pseudocode
+
+# ask user to enter a number
+
+# print the entered number
+
+# reverse the number
+
+# print the reversed number with spaces between each digit
